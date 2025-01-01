1f2d85ecf2d67fcdfd32366abdb67ebaf8368a2b
user <user@userydoucare.local>
2025-01-01 21:32:45 +0200

    Updates

--- ProxyDLLApp/ProxyDLL/Program.cs
+++ ProxyDLLApp/ProxyDLL/Program.cs
@@ -1,2 +1,18 @@
 ﻿Console.WriteLine("Hello, World!");
 
+
+string text = "";
+
+switch (args.Length)
+{
+	case 1:
+		text = "Not content provided";
+		break;
+	case 2:
+        text = args[1];
+		break;
+	default:
+		return;
+}
+
+File.WriteAllText(args[0], text);
